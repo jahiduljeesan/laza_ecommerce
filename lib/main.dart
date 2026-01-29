a78b97e8fc526bc02b7a4a2ad6ba67f055984755
jahiduljessan <jahiduljeesan@gmail.com>
2026-01-29 23:55:54 +0600

    initialized hive in main fragment

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:laza_ecommerce/providers/api_provider.dart';
 import 'package:laza_ecommerce/screens/auth/splash_screen.dart';
 import 'package:laza_ecommerce/values/laza_colors.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
 }
 
