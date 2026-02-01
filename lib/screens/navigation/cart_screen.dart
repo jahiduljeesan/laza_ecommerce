fba4feb69d392a2500d60727c4441ccb224078bc
jahiduljessan <jahiduljeesan@gmail.com>
2026-02-01 23:30:04 +0600

    updated cart screen

--- lib/screens/navigation/cart_screen.dart
+++ lib/screens/navigation/cart_screen.dart
@@ -5,6 +5,9 @@ import 'package:laza_ecommerce/values/laza_colors.dart';
 import 'package:laza_ecommerce/widgets/auth_widgets/title_text.dart';
 import 'package:laza_ecommerce/widgets/styles/cart_style.dart';
 import 'package:provider/provider.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+import '../../widgets/auth_widgets/bottom_button.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -30,69 +33,119 @@ class _CartScreenState extends State<CartScreen> {
       body: Consumer<CartProvider>(
         builder: (context, cart, _) {
           debugPrint("CartLength Cart Page: ${cart.cartItems.length}");
-          return SingleChildScrollView(
-            child: Column(
-              children: [
-                ListView.builder(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.vertical,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: cart.cartResponse?.totalProducts ?? 0,
-                  itemBuilder: (context, index) {
-                    final cartProduct = cart.cartResponse?.products[index];
-                    return CartStyle(
-                      cartItem: cartProduct,
-                      onDecrease: () => cart.updateQuantity(cartProduct?.id??0, false),
-                      onIncrease: () => cart.updateQuantity(cartProduct?.id??0, true),
-                      onDelete: () {
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            return AlertDialog(
-                              backgroundColor: Colors.white,
-                              icon: Image.asset(
-                                'assets/icons/laza_logo.png',
-                                color: LazaColors.primaryColor,
-                                height: 25,
-                                width: 25,
-                              ),
-                              title: Text('Do you really want to delete?'),
-                              actions: [
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: Text(
-                                    'Cancel',
-                                    style: TextStyle(
-                                      color: LazaColors.primaryColor,
-                                      fontSize: 16,
-                                    ),
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: cart.cartResponse?.totalProducts ?? 0,
+                itemBuilder: (context, index) {
+                  final cartProduct = cart.cartResponse?.products[index];
+                  return CartStyle(
+                    cartItem: cartProduct,
+                    onDecrease: () =>
+                        cart.updateQuantity(cartProduct?.id ?? 0, false),
+                    onIncrease: () =>
+                        cart.updateQuantity(cartProduct?.id ?? 0, true),
+                    onDelete: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            backgroundColor: Colors.white,
+                            icon: Image.asset(
+                              'assets/icons/laza_logo.png',
+                              color: LazaColors.primaryColor,
+                              height: 25,
+                              width: 25,
+                            ),
+                            title: Text('Do you really want to delete?'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text(
+                                  'Cancel',
+                                  style: TextStyle(
+                                    color: LazaColors.primaryColor,
+                                    fontSize: 16,
                                   ),
                                 ),
-                                TextButton(
-                                  onPressed: () {
-                                    cart.deleteItem(cartProduct?.id??0);
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: Text(
-                                    'Okay',
-                                    style: TextStyle(
-                                      color: LazaColors.primaryColor,
-                                      fontSize: 16,
-                                    ),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  cart.deleteItem(cartProduct?.id ?? 0);
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text(
+                                  'Okay',
+                                  style: TextStyle(
+                                    color: LazaColors.primaryColor,
+                                    fontSize: 16,
                                   ),
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                  );
+                },
+              ),
+              Container(
+                color: Colors.white,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Skeletonizer(
+                      enabled: cart.isLoading,
+                      enableSwitchAnimation: true,
+                      child: Container(
+                        padding: const EdgeInsets.all(16),
+                        color: Colors.white,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text(
+                                  'Total Price',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                Text(
+                                  'with VAT, SD',
+                                  style: TextStyle(fontSize: 13),
                                 ),
                               ],
-                            );
-                          },
-                        );
+                            ),
+                            Text(
+                              '',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    BottomButton(
+                      label: 'Add to Cart',
+                      onTap: () {
                       },
-                    );
-                  },
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              )
+            ],
           );
         },
       ),
